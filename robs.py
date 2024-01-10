e28d8999bb27799176340cd67076a4fe6171e1e1
Ren Iker <kumikoren@jcom.zaq.ne.jp>
2024-01-11 03:49:35 +0900

    robs

--- robs.py
+++ robs.py
@@ -3,11 +3,11 @@
 import random
 
 def display_random_color():
-    colors = ['白', '黒', '赤']
-    occasions = ['頭', '上半身', '下半身']
-    for i in range(3):
+    colors = ['白', '黒', '赤', '黄', 'ベージュ', '緑', '茶', '紺', '青', 'オレンジ', '紫']
+    occasions = ['帽子', '上半身', '下半身' '靴'] 
+    for i in range(4):
         random_color = random.choice(colors)
-        print(f'{occasions[i]}のランダムな色: {random_color}')
+        print(f'{occasions[i]}: {random_color}')
 
 if __name__ == "__main__":
     display_random_color()
