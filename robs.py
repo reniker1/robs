e6189efb72606469226ac7d5a6d0bf9ae6d8eb01
Ren Iker <kumikoren@jcom.zaq.ne.jp>
2024-01-11 04:53:14 +0900

    robs

--- robs.py
+++ robs.py
@@ -3,11 +3,20 @@
 import random
 
 def display_random_color():
-    colors = ['白', '黒', '赤', '黄', 'ベージュ', '緑', '茶', '紺', '青', 'オレンジ', '紫']
-    occasions = ['帽子', '上半身', '下半身' '靴'] 
-    for i in range(4):
-        random_color = random.choice(colors)
-        print(f'{occasions[i]}: {random_color}')
+    category = ['カジュアル', 'スポーツ', 'ストリート']
+    colors = ['白', '黒', '赤', 'グレー', 'ベージュ', '青', '緑', '紫', 'オレンジ']
+    head_colors = ['無', 'ニット', 'キャップ', ]
+    body_parts = ['帽子', 'ウェア', 'パンツ', 'シューズ']
+    
+    for i, part in enumerate(body_parts):
+        if part == '帽子':
+            random_color = random.choice(head_colors)
+        else:
+            random_color = random.choice(colors)
+        print(f'{part}の色: {random_color}'}
+
+    random_category = random.choice(category)
+    print(f'最後のスタイル: {random_category}')
 
 if __name__ == "__main__":
     display_random_color()
