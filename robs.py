54e0ec16c6c66db45a27920f5195dd95ae20f1af
Ren Iker <kumikoren@jcom.zaq.ne.jp>
2024-01-11 03:19:24 +0900

    robs

--- robs.py
+++ robs.py
@@ -1,20 +1,12 @@
 #!/usr/bin/python3
 
 import random
-import string
 
-def generate_random_string(length):
-    # ランダムな文字列を生成する関数
-    letters = string.ascii_letters + string.digits  # 英字と数字を使用します
-    return ''.join(random.choice(letters) for i in range(length))
-
-def generate_random_email(domain='example.com'):
-    # ランダムなメールアドレスを生成する関数
-    username_length = random.randint(5, 12)  # ユーザー名の長さをランダムに選択
-    username = generate_random_string(username_length)
-    email = f'{username}@{domain}'
-    return email
+def display_random_color():
+    colors = ['白', '黒', '赤']
+    for i in range(1, 4):
+        random_color = random.choice(colors)
+        print(f'{i}回目のランダムな色: {random_color}')
 
 if __name__ == "__main__":
-    random_email = generate_random_email()
-    print(f"Random Email Address: {random_email}")
+    display_random_color()
